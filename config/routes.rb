e21662388ffc15e25adb3871cc230956b061a3a7
vieiramanda11 <amanda_vieira@outlook.com>
2019-10-24 19:44:00 -0300

    Refactoring home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   post 'signup', to: 'users#create'
-  get 'show/:id', to: 'users#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
