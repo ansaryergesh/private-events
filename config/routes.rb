086d30a67988babff5ffb61932816afe328eff9a
Ansar <yergeshovansar@gmail.com>
2019-10-25 04:05:31 +0600

    session controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
-  get 'users/new', to: 'users#new'
-  get 'users/create', to: 'users#create'
-  get 'users/show/:id', to: 'users#show'
+  get '/signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get 'show/:id', to: 'users#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
