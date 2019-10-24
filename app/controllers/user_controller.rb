61b1bdeb18f9ff3dcefacde2f2228e7fcce95e2c
Ansar <yergeshovansar@gmail.com>
2019-10-25 02:27:58 +0600

    user controller created

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash.now[:success] = 'Welcome to the Events App'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'user/new', to: 'user#new'
+  get 'user/create', to: 'user#create'
+  get 'user/show', to: 'user#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
