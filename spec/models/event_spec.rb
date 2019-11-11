422fe240a50d0f20ddc99fe82301ca22c0020b64
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-11 17:59:38 -0300

    writing tests for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: "Name") }
+  before { subject.save }
+
+  it "should be valid" do
+    event = subject.events.create(location: 'location', event: 'event', description: 'desc', date: '12.02.2020')
+    expect(event).to be_valid
+  end
+
+  it "name should be present" do
+    event = subject.events.create(location: nil, event: 'event', description: 'desc', date: '12.02.2020')
+    expect(event).to_not be_valid
+  end
 end
